4241876cd85d708260b090ba0447ea4d70d4d6fb
ifeoluwa <ifeoluwaisaiah@gmail.com>
2023-08-11 22:06:29 +0100

    initial commit

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
     return (
       <header className="bg-white h-20 w-full shadow-xl flex items-center justify-between px-6">
-        <Link to="/home" className="font-bold text-2xl uppercase">#LimoDrive</Link>
+        <Link to="/home" className="font-bold text-2xl uppercase">#LimoDriveway</Link>
         <nav className="space-x-4 text-lg">
         <Link to="/about">About</Link>
         <Link to="/home">Vans</Link>
